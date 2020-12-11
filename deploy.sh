0bf1eae1552aff490e74ba4938c72cc4aa43542d
isaachammes <53546318+isaachammes@users.noreply.github.com>
2020-12-11 01:06:35 -0600

    Delete deploy.sh

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-kubectl apply -f rabbitmq/rabbitmq-deployment.yaml
-kubectl apply -f rabbitmq/rabbitmq-service.yaml
-kubectl apply -f worker/worker-deployment.yaml
-kubectl apply -f rest/rest-deployment.yaml
-kubectl apply -f rest/rest-service.yaml
-kubectl apply -f database/mysql-deployment.yaml
-kubectl apply -f database/mysql-service.yaml
